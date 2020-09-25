24db9e920dadca0020e191ed28ce4e0b15a344f8
Gayla Cloud <gaylacloud@Gaylas-MacBook-Pro.local>
2020-09-25 07:50:35 -0500

    Commit 2. All 3 tables are good for viewing.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //Dependencies
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
+const connection = require("./db/connection.js")
+    
 
 
 //Function to start questions
@@ -12,12 +14,12 @@ function start() {
       message: "What would you like to do",
       choices: [
                 "View All Employees",
+                "View Roles",
+                "View Departments",
+                "Update Employee Role",
                 "Add An Employee",
                 "Remove An Employee",
-                "Update Employee Role",
                 "Update Employee Manager",
-                "View Employees by Manager",
-                "View Employees by Department",
                 "View Salaries Made by Departement",
                 ]
     })
@@ -25,19 +27,19 @@ function start() {
         // based on their answer
         switch (answer.One) {
             case "View All Employees":
-                viewAll();
+                viewAllEmp();
                 break;
         }
 
         switch (answer.One) {
-            case "Add An Employees":
-                addE();
+            case "View Roles":
+                viewRoles();
                 break;
         }
 
         switch (answer.One) {
-            case "Remove An Employee":
-                removeE();
+            case "View Departments":
+                viewDept();
                 break;
         }
 
@@ -48,19 +50,19 @@ function start() {
         }
 
         switch (answer.One) {
-            case "Update Employee Manager":
+            case "Add An Employee":
                 updateEman();
                 break;
         }
 
         switch (answer.One) {
-            case "View Employees by Manager":
+            case "Remove An Employee":
                 viewEman();
                 break;
         }
 
         switch (answer.One) {
-            case "View Employees by Department":
+            case "Update Employee Manager":
                 viewDep();
                 break;
         }
@@ -73,5 +75,29 @@ function start() {
       });
 };
 
+// This allow users to see all employees in database 
+function viewAllEmp() {
+    console.log("Getting all emplyees");
+    var allEmpys =
+      "SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.name AS department, roles.salary FROM employee LEFT JOIN roles ON employee.roles_id = roles.id LEFT JOIN department on roles.department_id = department.id;";
+    connection.query(allEmpys, function(err, answer) {
+      console.log("\n Got ALL!");
+      console.table(answer);
+    });
+  };
 
+// This allows user to view all employee roles in the database
+function viewRoles() {
+    connection.query("SELECT * FROM roles", function(err, answer) {
+      console.log("\n Got the roles? Yes!");
+      console.table(answer);
+    });
+  }
+
+function viewDept(){
+    connection.query("SELECT * FROM department", function(err, answer) {
+        console.log("\n Departments? Yup!");
+        console.table(answer);
+    } );
+};
 start();
